1e71999e1dfc5df870fe9b9f91341f8f1bb640e3
harshitkandpal <harshitheroh5@gmail.com>
2025-07-24 23:08:06 +0530

    feat: update database settings to use PostgreSQL and add psycopg2-binary dependency

--- backend/CodeCanvas_User_Module/settings.py
+++ backend/CodeCanvas_User_Module/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'CodeCanvas_User_Module.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'mypassword',
+        'HOST': '192.168.241.143',
+        'PORT': '5432'
     }
 }
 
