e635bd6f8148f12d2ba01591b88c1b367190e998
harshitkandpal <harshitheroh5@gmail.com>
2025-07-25 08:49:38 +0530

    feat: add user authentication with Django REST framework and create hello endpoint

--- backend/CodeCanvas_User_Module/settings.py
+++ backend/CodeCanvas_User_Module/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',  # optional, for built-in auth endpoints
     'users.apps.UsersConfig',
 ]
 
@@ -125,3 +128,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from rest_framework import generics, status, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+# from rest_framework.simplejwt.tokens import RefreshToken
+
 
 # Create your views here.
+@api_view(['GET'])
+def hello(request):
+    return Response({"message": "Hello World"})
+    
