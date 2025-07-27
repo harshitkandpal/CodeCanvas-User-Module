1e021db8ce2090e036fd521c63c315e10d9b71fd
harshitkandpal <harshitheroh5@gmail.com>
2025-07-27 08:02:34 +0530

    feat: replace djoser with rest_framework_simplejwt for authentication and enhance utils with OTP generation and email sender functions

--- backend/CodeCanvas_User_Module/settings.py
+++ backend/CodeCanvas_User_Module/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'djoser',  # optional, for built-in auth endpoints
+    'rest_framework_simplejwt',
     'users.apps.UsersConfig',
 ]
 
--- backend/users/utils.py
+++ backend/users/utils.py
@@ -1,2 +1,18 @@
-#unique_otp_generator, email_sender, verify_otp, refresh_Token_gnerator, access_toker_generator, token_manger(add remove expiry token regenerates etc) 
+from random import randint
+import os
+
+#otp_generator, email_sender, verify_otp, refresh_Token_gnerator, access_toker_generator, token_manger(add remove expiry token regenerates etc) 
 #OAuth in future
+
+def otp_generator ():
+    otp = ""
+    for i in range(os.environ.get('OTP_LENGTH', 8)):
+        otp += str(randint(0,9))
+    
+    return otp
+
+def email_sender(email,otp,content):
+
+    return True
+
+
